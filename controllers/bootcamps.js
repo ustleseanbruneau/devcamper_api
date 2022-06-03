18b2264f8981b38b79a453da7c4f341b371941f4
LB <eagandog2009@gmail.com>
2022-06-03 08:31:41 -0500

    S33 - Geospatial Query add route search by distance

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
+const geocoder = require('../utils/geocoder')
 const Bootcamp = require('../models/Bootcamp')
 
 // @desc    GET all bootcamps
@@ -93,5 +94,35 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     }
   
     res.status(200).json({ success: true, data: {} })
+
+}) 
+
+// @desc    Get bootcamp
+// @route   GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access  Private
+// Test:  http://localhost:5000/api/v1/bootcamps/radius/02118/10
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  // Get lat/lng from geocoder
+  const loc = await geocoder.geocode(zipcode)
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  // Calc radius using radians
+  // Divide dist by radius of Earth
+  //  Earth Radius = 3,963 miles / 6,378 km
+  const radius = distance / 3963
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [ [ lng, lat ], radius ] } }
+  })
+
+  res.status(200).json({
+    success: true, 
+    count: bootcamps.length,
+    data: bootcamps
+  })
+
 
 }) 
