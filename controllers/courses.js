fb3a89fbac0f1bf6d0bb76465b2d89f44bc3322c
UST L.B <eagandog2009@gmail.com>
2022-06-03 14:11:07 -0500

    S39 - Populate Virtuals and Cascade Delete

--- controllers/courses.js
+++ controllers/courses.js
@@ -12,7 +12,12 @@ exports.getCourses = asyncHandler(async (req,res,next) => {
     if(req.params.bootcampId) {
         query = Course.find({ bootcamp: req.params.bootcampId })
     } else {
-        query = Course.find()
+        //query = Course.find()
+        //query = Course.find().populate('bootamp')
+        query = Course.find().populate({
+            path: 'bootcamp',
+            select: 'name description'
+        })
     }
 
     const courses = await query;
